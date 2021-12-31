60c0399240a9994adb0c03b5a0acd14b313eb764
Andrew Krasnoff <andrew.krasnov1979@gmail.com>
2021-12-31 15:48:05 +0200

    tags cloud added

--- app/models/tag.rb
+++ app/models/tag.rb
@@ -3,7 +3,7 @@ class Tag < ApplicationRecord
   has_many :posts, through: :taggings
 
   def self.counts
-    self.select("name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id").reverse_order!
+    self.select("tags.name, count(taggings.tag_id) as count").joins(:taggings).group("taggings.tag_id").reverse_order!
   end
 
 end
